f79d8632b979e7a6200af4246d398bfaae31ab5d
neil399399 <neil399399@gmail.com>
2022-03-26 01:05:03 +0800

    fix bug

--- domain/services/user.go
+++ domain/services/user.go
@@ -2,7 +2,7 @@ package services
 
 import "Hephaestus/models"
 
-func GetUser(username string, password string) (models.User, error) {
+func GetUser(username string, password string) error {
 	user := models.User{}
 	user.Username = username
 	user.Password = password
